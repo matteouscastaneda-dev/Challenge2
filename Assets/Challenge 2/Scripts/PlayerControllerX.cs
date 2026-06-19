a9be8efbb5ba5332b78f1a017596e1e2a666c40d
MatteousCastaneda <matteous.castaneda@g.austincc.edu>
2026-06-19 16:14:48 -0500

    Updated PlayerControllerX.cs to include a spawn interval.
    Added a couple of float variables to keep track of spawn time and compare that to unity's Time.time. now the every time the player presses the spacebar it checks if the time elapsed is enough.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,6 +6,9 @@ public class PlayerControllerX : MonoBehaviour
     public GameObject dogPrefab;
     public InputAction fireAction;
 
+    private float spawnInterval = 1.5f;
+    private float nextSpawnTime = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +19,10 @@ public class PlayerControllerX : MonoBehaviour
     void Update()
     {
         // On spacebar press, send dog
-        if (fireAction.triggered)
+        if (fireAction.triggered && Time.time >= nextSpawnTime)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            nextSpawnTime = Time.time + spawnInterval;
         }
     }
 }
